13da4e0c371d4238f152a5b06722499162771817
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-09-19 12:51:55 -0400

    re-indent

--- week01/textart.playground/Contents.swift
+++ week01/textart.playground/Contents.swift
@@ -3,33 +3,33 @@ import Foundation
 let str = "#*+.:|/\\-=_"
 
 func charAt(_ str:String, _ offset:Int) -> String {
-let index = str.index(str.startIndex, offsetBy: offset)
-let char = str[index]
-return String(char)
+  let index = str.index(str.startIndex, offsetBy: offset)
+  let char = str[index]
+  return String(char)
 }
 
 func generateLine(_ length: Int) {
-    var line = ""
-    for _ in 0..<length {
-        let randomInt = Int.random(in: 0..<str.count)
-        line += charAt(str, randomInt)
-    }
-    print(line)
+  var line = ""
+  for _ in 0..<length {
+    let randomInt = Int.random(in: 0..<str.count)
+    line += charAt(str, randomInt)
+  }
+  print(line)
 }
 
 func generateBlock(_ size: Int) {
-    for _ in 0..<size {
-        generateLine(size)
-    }
+  for _ in 0..<size {
+    generateLine(size)
+  }
 }
 
 func generatepattern(_ size: Int) {
-let chars = Array(str)
-for row in 0..<size {
-let ch = chars[row % chars.count]
-let line = String(repeating: ch, count: size)
-print(line)
-}
+  let chars = Array(str)
+  for row in 0..<size {
+    let ch = chars[row % chars.count]
+    let line = String(repeating: ch, count: size)
+    print(line)
+  }
 }
 
 generateBlock(10)
